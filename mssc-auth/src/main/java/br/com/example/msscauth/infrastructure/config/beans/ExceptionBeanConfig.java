806493a133b5aa0a351a5f27fb276cdeddcb090d
GabrielBergamim <gabriel.bergamim@outlook.com>
2023-08-27 20:38:40 -0300

    refactor: change handle exception controller advice to avoid boilerplate code

---
+++ mssc-auth/src/main/java/br/com/example/msscauth/infrastructure/config/beans/ExceptionBeanConfig.java
@@ -0,0 +1,23 @@
+package br.com.example.msscauth.infrastructure.config.beans;
+
+import br.com.example.msscauth.domain.exceptions.InvalidLoginException;
+import br.com.example.msscauth.domain.exceptions.MissingParameterException;
+import br.com.example.msscauth.domain.exceptions.UserValidationException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+
+@Configuration
+public class ExceptionBeanConfig {
+
+    @Bean
+    public Map<Class<? extends Exception>, HttpStatus> exceptionToStatusCode() {
+        return Map.of(
+                MissingParameterException.class, HttpStatus.BAD_REQUEST,
+                UserValidationException.class, HttpStatus.BAD_REQUEST,
+                InvalidLoginException.class, HttpStatus.UNAUTHORIZED
+        );
+    }
+}
