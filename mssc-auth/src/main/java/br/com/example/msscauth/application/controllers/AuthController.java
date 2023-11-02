1a49787d2155bf1f440bea0de0cc0997b827dcee
GabrielBergamim <gabriel.bergamim@outlook.com>
2023-11-02 14:24:07 -0300

    feat: add userDetails endpoint

--- mssc-auth/src/main/java/br/com/example/msscauth/application/controllers/AuthController.java
+++ mssc-auth/src/main/java/br/com/example/msscauth/application/controllers/AuthController.java
@@ -1,12 +1,15 @@
 package br.com.example.msscauth.application.controllers;
 
 import br.com.example.msscauth.domain.dto.login.LoginInputDto;
+import br.com.example.msscauth.domain.dto.user.UserOutputDto;
 import br.com.example.msscauth.domain.models.AuthorizationToken;
 import br.com.example.msscauth.domain.usecases.auth.AuthUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -25,5 +28,14 @@ public class AuthController {
     @GetMapping("/validate")
     public ResponseEntity<Void> validate() {
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/userDetails")
+    public ResponseEntity<UserOutputDto> userDetails() {
+        return ResponseEntity.ok(UserOutputDto.builder()
+                .email("user_email@email.com")
+                .id(1L)
+                .roles(Collections.singletonList("ROLE_ADMIN"))
+                .build());
     }
 }
